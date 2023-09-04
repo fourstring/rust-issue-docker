5ec9663cd8e124655e566cf9486a58bfc90508fb
Chunwei Zhou <fourstring@sjtu.edu.cn>
2023-09-04 19:28:24 +0800

    update to latest error-prone config.toml

--- build_rustc.sh
+++ build_rustc.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-RUSTFLAGS="-C target-feature=-crt-static"
-
-RUSTFLAGS_NOT_BOOTSTRAP=${RUSTFLAGS} RUSTFLAGS_BOOTSTRAP=${RUSTFLAGS} ./x build -v library/std
+./x build -v library/std
--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-RUSTFLAGS="-C target-feature=-crt-static"
-
-RUSTFLAGS_NOT_BOOTSTRAP=${RUSTFLAGS} RUSTFLAGS_BOOTSTRAP=${RUSTFLAGS} ./x test --stage 1 -v library/std
+./x test --stage 1 -v library/std
